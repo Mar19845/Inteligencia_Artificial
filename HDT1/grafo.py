2645cf0e41c87dda034238f34080cd151b3681ee
Mar19845 <mar19845@uvg.edu.gt>
2022-02-19 00:22:31 -0600

    ejercicio 1 terminado

---
+++ HDT1/grafo.py
@@ -0,0 +1,78 @@
+from collections import defaultdict
+from turtle import distance
+from helpers import *
+
+inf = float('inf')
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+    
+    def addNodes(self,list_nodes):
+        for node in list_nodes:
+            self.nodes.add(node)
+    
+    def addEdges(self,aristas):
+        for arista in aristas:
+            self.edges[arista[0]].append(arista[1])
+            self.distances[(arista[0], arista[1])] = arista[2]
+        
+n,m,aristas = read_file('grafo.txt')
+_,nodes = create_graph(aristas)
+
+G = Graph()
+G.addNodes(nodes)
+G.addEdges(aristas)
+#print(G.nodes)
+#print(G.edges)
+#print(G.distances)
+
+
+def dijkstra(graph, initial,end):
+    visited = {initial : 0}
+    path = defaultdict(list)
+
+    nodes = set(graph.nodes)
+
+    while nodes:
+        minNode = None
+        for node in nodes:
+            if node in visited:
+                if minNode is None:
+                    minNode = node
+                elif visited[node] < visited[minNode]:
+                    minNode = node
+        if minNode is None:
+            break
+        nodes.remove(minNode)
+        currentWeight = visited[minNode]
+        for edge in graph.edges[minNode]:
+            weight = currentWeight + graph.distances[(minNode, edge)]
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge].append(minNode)
+    
+    
+    camino = [end]
+    if visited[end] < inf:
+        while initial not in camino:
+            last_index = camino[-1]
+            val = path[last_index]
+            camino.append(val[0])
+    distancia = visited[end]
+    return distancia,camino
+
+print(dijkstra(G,'1','5'))
+
+import networkx as nx
+n,m,aristas = read_file('grafo.txt')
+G = nx.Graph()
+G.add_weighted_edges_from(aristas)
+
+print('-----------------------')
+print(nx.dijkstra_path(G,'1','5'))
+print(nx.dijkstra_path_length(G,'1','5'))
+
--- HDT1/helpers.py
+++ HDT1/helpers.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 def read_file(filename):
@@ -15,7 +16,9 @@ def read_file(filename):
     aristas = []
     for line in lines:
         #split vaules in line and convert to int
-        results = list(map(int, line.split(',')))
+        #results = list(map(int, line.split(',')))
+        results = line.split(',')
+        results[2] = int(results[2])
         #append results list to aristas
         aristas.append(results)
     
@@ -23,5 +26,18 @@ def read_file(filename):
     #aristas = np.array(aristas)
     return n, m, aristas
 
-
-print(*read_file('grafo.txt'))
+def create_graph(aristas):
+    graph = {}
+    
+    #agregar nodos al grafo
+    for arista in aristas:
+        graph[arista[0]] = {}
+        graph[arista[1]] = {}
+        #graph[arista[0]][arista[1]]=arista[2]
+    
+    #agregar las aristas a los nodoos, por cada nodo se crea un dict con key = al nodo que se dirige y el value es el peso
+    for i in aristas:
+        graph[i[0]][i[1]]=i[2]
+    
+    #return dict
+    return graph, list(graph.keys())
