769ae545962da8329c7f17805a77119fa5ed4379
eduardordev <eduardorh1312@gmail.com>
2022-02-20 22:26:53 -0600

    Hacer remplazo de 0 segun las posiciones CTM

--- HDT1/Ej2/sudoku.py
+++ HDT1/Ej2/sudoku.py
@@ -1,2 +1,18 @@
-def sudokuDFS(n,m,p1,p2,p3):
-    return("pruebas")
+def sudokuDFS(n, m, positions):
+    n = int(n)
+    sudoku = []
+    
+    for i in range(0,n):
+        row = []
+        for j in range(0,n):
+            row.append(0)
+        
+        sudoku.append(row)
+    
+    end = len(positions)
+
+    for i in range(0, end):
+        x, y, c = positions[i].split()
+        print(x,y,c)
+        
+    
--- HDT1/Ej2/sudokumain.py
+++ HDT1/Ej2/sudokumain.py
@@ -2,9 +2,9 @@ from cleaner import *
 
 print("****IA SUDOKU****\n")
 st = True
-f = open('4dms.txt', 'r')
-f1 = open('6dms.txt', 'r')
-f2 = open('9dms.txt', 'r')
+f = "4dms.txt"
+f1 = "6dms.txt"
+f2 = "9dms.txt"
 
 while(st):
     dim = input("Elija la dimension del sudoku que desea que la IA resuelva (UNICAMENTE 4,6 ó 9): ")
