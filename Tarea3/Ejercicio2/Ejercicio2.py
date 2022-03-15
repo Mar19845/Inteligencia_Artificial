8aa3f0e34cbde582a275071ebcfc13d3cab98293
Carlos21122000 <craxtum1@gmail.com>
2022-03-15 10:31:09 -0600

    avance del 2

--- Tarea3/Ejercicio2/Ejercicio2.py
+++ Tarea3/Ejercicio2/Ejercicio2.py
@@ -1,38 +1,66 @@
-from sys import maxsize
-from itertools import permutations
-V = 4
- 
+import numpy as np
+from itertools import combinations
+def TSP_SA(G):
+    s = list(range(len(G)))
+    c = cost(G, s)
+    ntrial = 1
+    T = 30
+    alpha = 0.99
 
-def travellingSalesmanProblem(graph, s):
- 
-    vertex = []
-    for i in range(V):
-        if i != s:
-            vertex.append(i)
- 
+    while ntrial <= 1000:
+      n = np.random.randint(0, len(G))
+      while True:
+         m = np.random.randint(0, len(G))
+         if n != m:
+            break
+      s1 = swap(s, m, n)
+      c1 = cost(G, s1)
+      if c1 < c:
+         s, c = s1, c1
+      else:
+         if np.random.rand() < np.exp(-(c1 - c)/T):
+            s, c = s1, c1
+      T = alpha*T
+      ntrial += 1
 
-    min_path = maxsize
-    next_permutation=permutations(vertex)
-    for i in next_permutation:
+def swap(s, m, n):
+   i, j = min(m, n), max(m, n)
+   s1 = s.copy()
+   while i < j:
+      s1[i], s1[j] = s1[j], s1[i]
+      i += 1
+      j -= 1
+   return s1
+ 
+def cost(G, s):
+   l = 0
+   for i in range(len(s)-1):
+      l += G[s[i]][s[i+1]]
+   l += G[s[len(s)-1]][s[0]] 
+   return l
 
-        current_pathweight = 0
 
-        k = s
-        for j in i:
-            current_pathweight += graph[k][j]
-            k = j
-        current_pathweight += graph[k][s]
- 
-
-        min_path = min(min_path, current_pathweight)
-         
-    return min_path
- 
- 
-# Driver Code
-if __name__ == "__main__":
- 
-    graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-            [15, 35, 0, 30], [20, 25, 30, 0]]
-    s = 0
-    print(travellingSalesmanProblem(graph, s))
+def read_file(filename):
+    #lines = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    
+    n = int(lines[0])
+    m = int(lines[1])
+    #remove n and m from list
+    lines.pop(0)
+    lines.pop(0)
+    
+    #create list wiht the info of aristas
+    aristas = []
+    for line in lines:
+        #split vaules in line and convert to int
+        #results = list(map(int, line.split(',')))
+        results = line.split(',')
+        results[2] = int(results[2])
+        #append results list to aristas
+        aristas.append(results)
+    
+    #convert to np array
+    #aristas = np.array(aristas)
+    return n, m, aristas
---
+++ Tarea3/Ejercicio2/entrada.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def pathReader():
+    count = 0
+    path = []
+    with open("array.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith('#'):
+                pass
+            else:
+                if count == 0:
+                    count += 1
+                elif count >= 1:
+                    split = line.split(' ')
+                    x = int(split[0])
+                    y = int(split[1])
+                    pos = [x, y]
+                    path.append(pos)
+                    
+    array = np.array(path)
+    print(array)
+    return array
+
+pathReader()
