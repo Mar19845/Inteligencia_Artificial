2e2a5702d51f2c78d06b9805caac62fa05e0a215
Mar19845 <mar19845@uvg.edu.gt>
2022-05-16 00:00:44 -0600

    ejercicio 2

---
+++ Tarea4/Ejercicio2/main.py
@@ -0,0 +1,21 @@
+from helpers import *
+
+#Create a sudoku 
+# pass data for the genetic algorithm 
+sudoku = Sudoku(
+        number_of_candidates=500,
+        number_of_elites=80,
+        number_of_generations=1000,
+        number_of_mutations=0,
+        selection_rate=0.85
+    )
+
+#load the sudoku from a file 
+sudoku.load('input.txt')
+
+#save the solution
+solution = sudoku.solve()
+
+if solution:
+    #save the solution in a txt file
+    sudoku.save('output.txt', solution)
